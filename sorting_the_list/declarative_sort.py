a94c844c3597f8d62a17049c94d38ea1779e8521
Denis Yershov <frontsteron@gmail.com>
2023-11-02 19:56:57 +0500

    fix: added sorting in imperative and declarative styl

---
+++ sorting_the_list/declarative_sort.py
@@ -0,0 +1,7 @@
+﻿def sort_list_declarative(numbers):
+    return sorted(numbers, reverse=True)
+
+
+numbers = [4, 2, 9, 1, 5]
+sorted_numbers = sort_list_declarative(numbers)
+print(sorted_numbers)  # Выведет [9, 5, 4, 2, 1]
---
+++ sorting_the_list/imperative_sort.py
@@ -0,0 +1,11 @@
+﻿def sort_list_imperative(numbers):
+    n = len(numbers)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if numbers[j] < numbers[j + 1]:
+                numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+
+
+numbers = [4, 2, 9, 1, 5]
+sort_list_imperative(numbers)
+print(numbers)  # Выведет [9, 5, 4, 2, 1]
